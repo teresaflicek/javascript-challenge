8470d0d652df447f36622c6760ed6ae9c5a040b9
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-01-01 13:53:23 -0600

    table appended

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// console.log the data from data.js
+console.log(tableData);
+
+// loop through the data and append the table to the web page
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = d3.select("tbody").append("tr");
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
+
+
+
+
+
+// date form for filter the events by date/time
+
