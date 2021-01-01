cab6ba2b223c7e1c03fdea7dba0c4c25cef550e5
teresaflicek <69260261+teresaflicek@users.noreply.github.com>
2021-01-01 14:50:40 -0600

    cleaned comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,17 +12,17 @@ function renderTable(data) {
         Object.entries(sightings).forEach(function ([key, value]) {
             console.log(key, value);
             // Append a cell to the row for each value
-            // in the tableData object
+            // in the data object
             var cell = row.append("td");
             cell.text(value);
         });
     });
 };
 
-// loop through the data and append the table to the web page
+// loop through the data and append the table with all sightings to the web page
 renderTable(tableData);
 
-//filtering the events by date/time
+// filtering the events by date/time
 
 // select the buttom
 var button = d3.select("#filter-btn");
@@ -30,34 +30,35 @@ var button = d3.select("#filter-btn");
 // select the form 
 var form = d3.select("form");
 
-// Create event handlers for clicking the button or pressing the enter key
+// create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
-// Create the function to run for both events
+// create the function to run for both events
 function runEnter() {
 
-    // Prevent the page from refreshing
+    // prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Select the input element and get the raw HTML node
+    // select the input element and get the raw HTML node
     var inputElement = d3.select(".form-control");
 
-    // Get the value property of the input element
+    // get the value property of the input element
     var inputValue = inputElement.property("value");
 
-    // Print the value to the console
+    // print the value to the console
     console.log(inputValue);
 
-    //filter the data
+    // filter the data
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
+    // print the values to the console
     console.log(filteredData);
 
     // clear the existing table
     d3.select("tbody").html("")
 
-    // loop through the data and append the table to the web page
+    // loop through the data and append the table with the filtered sightings to the web page
     renderTable(filteredData);
 
-}
+};
